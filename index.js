00adedde9e20953a8ead4f040b3057c7ba5beaf7
jaywcjlove <398188662@qq.com>
2018-05-05 09:36:07 +0800

    Add files.

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = require('./lib/index.js');
+
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./lib/index.js');
+// } else {
+//   module.exports = require('./lib/index.js');
+// }
